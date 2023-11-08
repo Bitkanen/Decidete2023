4597506af19d9d709b533ef6d89172b248dcabd4
Issac Vargas <edgar24_vargas@hotmail.com>
2023-11-07 18:47:45 -0700

    Se agrego pagina web

---
+++ PaginaWeb - Issac/UniversidadBinacionalWeb/Pages/Carrers.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniversidadBinacionalWeb.Pages
+{
+	public class CarrersModel : PageModel
+	{
+		private readonly ILogger<CarrersModel> _logger;
+
+		public CarrersModel(ILogger<CarrersModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
